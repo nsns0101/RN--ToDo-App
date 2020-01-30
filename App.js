28dfda26ab5aa6b33477abd5209efc1edd79dc96
nsns0101 <nsns0101@naver.com>
2020-01-30 21:21:35 +0900

    create_ToDo.js, ScrollView

--- App.js
+++ App.js
@@ -1,19 +1,84 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar, TextInput, Dimensions, Platform, ScrollView } from 'react-native';
+import { render } from 'react-dom';
+import ToDo from "./ToDo"
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+const { width, height } = Dimensions.get("window"); //현재 휴대폰의 가로 세로를 가져옴
+
+export default class App extends React.Component {
+  state = {
+    newToDo: "",
+  };
+  render() {
+    const { newToDo } = this.state
+    return (
+      <View style={styles.container}>
+        <StatusBar barStyle="light-content" />
+        <Text style={styles.title}>ToDo App</Text>
+        <View style={styles.card}>
+          <TextInput
+            style={styles.inputText}
+            placeholder={"New To Do"}
+            placeholderTextColor={"#999"}
+            value={newToDo}
+            onChangeText={this._crontollNewToDo}
+            returnKeyType={"done"}
+            autoCorrect={false}   //글 자동 수정
+          />
+
+          <ScrollView>
+            <ToDo />
+          </ScrollView>
+        </View>
+      </View>
+    );
+  }
+
+  _crontollNewToDo = text => {
+    this.setState({
+      newToDo: text
+    });
+  };
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#F23657',
     alignItems: 'center',
-    justifyContent: 'center',
+    // justifyContent: 'center'
   },
+  title: {
+    color: "white",
+    fontSize: 30,
+    marginTop: 50,
+    fontWeight: "100",          //글의 두께
+    marginBottom: 30
+  },
+  card: {
+    backgroundColor: "white",
+    flex: 1,
+    width: width - 25,          // 가로 크기
+    borderTopLeftRadius: 10,    //왼쪽 상단의 테두리 모양
+    borderTopRightRadius: 10,    //오른쪽 상단의 테두리 모양
+    ...Platform.select({
+      ios: {      // ios인 경우
+        shadowColor: "rgb(50, 50, 50)",
+        shadowOpacity: 0.5,
+        shadowRadius: 5,
+        shadowOffset: {
+          width: 0,
+          height: -1
+        }
+      },
+      android: {    // android인 경우
+        elevation: 3
+      }
+    }),
+  },
+  inputText: {
+    padding: 20,
+    borderBottomColor: "#bbb",
+    borderBottomWidth: 1,
+  }
 });
---
+++ README.md
@@ -0,0 +1,19 @@
+## > ToDo App <
+- 할일 리스트 체크 App
+
+# 참고 강의 사이트
+- https://academy.nomadcoders.co/courses/enrolled/303211
+
+# NPM, Android Studio 설치
+- NPM : https://nodejs.org/ko/download/
+- Android Studio : https://developer.android.com/studio?hl=ko
+
+# Expo-cli 설치
+- npm install -g expo-cli
+
+# Expo 프로젝트 생성
+- expo init ProjectName
+
+# 프로젝트 실행
+- 1. AndroidStudio 에뮬레이터 실행
+- 2. npm start, expo start로 프로젝트 실행(package.json 참고)
