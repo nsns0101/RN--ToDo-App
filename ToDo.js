11c2c727ba83199187212e7adf3cfee14a7931be
nsns0101 <nsns0101@naver.com>
2020-02-03 17:42:32 +0900

    insert_icon

--- ToDo.js
+++ ToDo.js
@@ -9,19 +9,56 @@ export default class ToDo extends React.Component {
         isCompleted: false,         //수정끝인지?
     }
     render() {
-        const { isCompleted } = this.state;
+        const { isCompleted, isEditing } = this.state;
         return (
-
             <View style={styles.container}>
-                {/* 클릭할 수 있는 태그   (onPress는 클릭하였을 경우) */}
-                <TouchableOpacity onPress={this._toggleComplete}>
-                    <View style={[
-                        styles.radio,
-                        // 라디오버튼이 클릭되면 styles.completeRadio 스타일을 먹임
-                        isCompleted ? styles.completedRadio : styles.uncompletedRadio
-                    ]} />
-                </TouchableOpacity>
-                <Text style={styles.text}>Hello I'm a To Do</Text>
+
+                <View style={styles.column}>
+                    {/* 클릭 라디오버튼 */}
+                    <TouchableOpacity onPress={this._toggleComplete}>
+                        {/* 스타일을 2개 먹이기 위해선 배열로 줘야 함 */}
+                        <View style={[
+                            styles.radio,
+                            // 라디오버튼이 클릭되면 styles.completeRadio 스타일을 먹임
+                            isCompleted ? styles.completedRadio : styles.uncompletedRadio
+                        ]} />
+                    </TouchableOpacity>
+
+                    {/* 클릭 텍스트 */}
+                    <TouchableOpacity onPress={this._toggleComplete}>
+                        <Text style={[styles.text, isCompleted ? styles.completedText : styles.uncompletedText]}>
+                            Hello I'm a To Do
+                        </Text>
+                    </TouchableOpacity>
+                </View>
+                {isEditing ? (
+                    <View style={styles.actions}>
+                        {/* 편집완료 버튼 */}
+                        <TouchableOpacity onPressOut={this._finishEditing}>
+                            <View style={styles.actionContainer}>
+                                <Text style={styles.actionText}>✅</Text>
+                            </View>
+                        </TouchableOpacity>
+                    </View>
+                ) : (
+                        <View style={styles.actions}>
+                            {/* 편집하기 버튼 */}
+                            <TouchableOpacity onPressOut={this._startEditing}>
+                                <View style={styles.actionContainer}>
+                                    <Text style={styles.actionText}>✏️</Text>
+                                </View>
+                            </TouchableOpacity>
+                            {/* 삭제 버튼*/}
+                            <TouchableOpacity>
+                                <View style={styles.actionContainer}>
+                                    <Text style={styles.actionText}>❌</Text>
+                                </View>
+                            </TouchableOpacity>
+                        </View>
+                    )
+                }
+
+
             </View>
         );
     }
@@ -34,21 +71,36 @@ export default class ToDo extends React.Component {
             };
         });
     };
+
+    _startEditing = () => {
+        this.setState({
+            isEditing: true
+        })
+    }
+
+    _finishEditing = () => {
+        this.setState({
+            isEditing: false
+        })
+    }
 }
+
+
 const styles = StyleSheet.create({
     container: {
         width: width - 50,
         borderBottomColor: "#bbb",
         borderBottomWidth: StyleSheet.hairlineWidth,
         flexDirection: "row",
-        alignItems: "center"
+        alignItems: "center",
+        justifyContent: "space-between"
     },
     text: {
         fontWeight: "600",
         fontSize: 20,
         marginVertical: 20
     },
-    radio: {
+    radio: {        //라디오 버튼
         width: 26,
         height: 26,         // 원을 그릴거라 가로세로가 같아야 함
         borderRadius: 13,   // 원을 그릴거라 가로세로의 반이여야 함
@@ -56,11 +108,36 @@ const styles = StyleSheet.create({
         borderColor: "red",
         borderWidth: 3,
         marginRight: 20,
+        // flexDirection: "row",
     },
-    completedRadio: {
-        borderColor: "#bbb"
+    completedRadio: {           //할일 완료시의 버튼(클릭했을 경우)
+        borderColor: "#bbb",
     },
-    uncompletedRadio: {
-        borderColor: "#F23657"
+    uncompletedRadio: {         //할일 미완료시의 버튼(클릭하지 않았을 경우)
+        borderColor: "#F23657",
+
     },
+    completedText: {            //할일 완료시의 텍스트
+        color: "#bbb",
+        textDecorationLine: "line-through",
+    },
+    uncompletedText: {
+        color: "black"
+    },
+    column: {
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+        width: width / 2,
+    },
+    actions: {
+        flexDirection: "row"
+    },
+    actionContainer: {
+        marginVertical: 10,
+        marginHorizontal: 10,
+    },
+    actionText: {
+
+    }
 });
